2e4d17714c3b1114f816ebd7d7723208b0b3b4d6
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-10-10 05:45:06 +0530

    uploading file to s3

---
+++ 02_AWS_Boto3_S3/02_aws_s3_upload_to_bucket.py
@@ -0,0 +1,16 @@
+import boto3
+import os
+
+s3_client = boto3.client('s3', region_name='ap-south-1')
+
+# bucket to upoad to 
+bucket_name = 'test93839'
+
+# file to upload
+file_path = './index.html'
+
+# extracts just the file or folder name from a full path — removing the directory part.
+object_name = os.path.basename(file_path)
+
+# upload to bucket
+s3_client.upload_file(file_path, bucket_name, object_name)
