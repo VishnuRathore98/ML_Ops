96fb9fd5ef459530cf621a73bdcf17808e41a789
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-10-12 22:51:22 +0530

    Sentiment classification -> load dataset

---
+++ 03_Sentiment_Classification_Using_TinyBERT_Transformer/01_load_data_HF_Dataset.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from datasets import Dataset, load_dataset
+from pprint import pprint
+import os
+import time
+
+
+url = 'https://raw.githubusercontent.com/laxmimerit/All-CSV-ML-Data-Files-Download/master/IMDB-Dataset.csv'
+local_dataset_path = 'IMDB-Dataset.csv'
+
+start = time.time()
+
+# Storing dataset locally such that data loading time could be decreased
+# Checking if dataset exists locally
+if not os.path.exists(local_dataset_path):
+    print('Downloading dataset...')
+    # Read the dataset from online url to memory
+    data = pd.read_csv(url)
+    # Save the csv file locally without indexing it
+    data.to_csv(local_dataset_path, index=False)
+else:
+    print('Loading cached data...')
+    # Reading using pandas does not cache in memory for faster access in subsequest accesses 
+    #data = pd.read_csv(local_dataset_path)
+    # Using dataset's load_dataset function for chaching and quick access 
+    dataset = load_dataset('csv', data_files=local_dataset_path)
+
+# Fetch the default trainig set 
+train_dataset = dataset['train']
+# Dividing into training and testing sets 
+dataset = train_dataset.train_test_split(test_size=0.3)
+# Reading as pandas dataframe
+data = pd.read_csv('IMDB-Dataset.csv')
+end = time.time()
+
+# Labeling the data 
+label2id = {'positive':1, 'negative':0}
+id2label = {1:'positive', 0:'negative'}
+
+# Using the map function to label the data in dataset 
+dataset = dataset.map(lambda x: {'label': label2id[x['sentiment']]})
+
+print(f'Time took for data loading and labeling : {end-start:.2f} seconds.')
+
+pprint(dataset['test'][0])
