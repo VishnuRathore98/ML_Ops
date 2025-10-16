2150058646f331689bc0d520d3dbf434d6842175
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-10-16 08:07:07 +0530

    upload model on s3

---
+++ 03_Sentiment_Classification_Using_TinyBERT_Transformer/07_upload_model_on_s3.py
@@ -0,0 +1,18 @@
+import boto3
+import os
+
+s3_client = boto3.client('s3',region_name='ap-south-1')
+bucket_name = "mlops-vpsr"
+model_path="TinyBERT-sentiment-analysis"
+s3_prefix="ml-models/TinyBERT-sentiment-analysis"
+
+def upload_model(model_path, s3_prefix):
+    for root, dirs, files in os.walk(model_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            rel_path = os.path.relpath(file_path, model_path)
+            s3_key = os.path.join(s3_prefix, rel_path)
+
+            s3_client.upload_file(file_path,bucket_name, s3_key)
+
+upload_model(model_path, s3_prefix)
